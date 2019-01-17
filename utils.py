45ca8e0eb8eb85d1255b627f93fc5c1b26531615
iarunava <learningdroidarunava@gmail.com>
2019-01-17 08:56:46 +0530

    Added comment to explain the function usage

--- utils.py
+++ utils.py
@@ -2,7 +2,31 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def create_class_mask(img, color_map, is_normalized_img=True, is_normalized_map=False):
+def create_class_mask(img, color_map, is_normalized_img=True, is_normalized_map=False, show_masks=False):
+    """
+    Function to create C matrices from the segmented image, where each of the C matrices is for one class
+    with all ones at the pixel positions where that class is present
+
+    img = The segmented image
+
+    color_map = A list with tuples that contains all the RGB values for each color that represents
+                some class in that image
+
+    is_normalized_img = Boolean - Whether the image is normalized or not
+                        If normalized, then the image is multiplied with 255
+
+    is_normalized_map = Boolean - Represents whether the color map is normalized or not, if so
+                        then the color map values are multiplied with 255
+
+    show_masks = Wherether to show the created masks or not
+    """
+    
+    if is_normalized_img and (not is_normalized_map):
+        img *= 255
+
+    if is_normalized_map and (not is_normalized_img):
+        img = img / 255
+    
     mask = []
     hw_tuple = img.shape[:-1]
     for color in color_map:
