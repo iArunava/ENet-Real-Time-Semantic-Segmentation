03ff0d44ca21e66779e40db74c9e522bc783a4ee
Arunava <learningdroidarunava@gmail.com>
2019-01-19 10:06:09 +0530

    Updated ENet class to be more readable

---
+++ models/enet.py
@@ -0,0 +1,115 @@
+#####################################################
+# Reproducing the paper                             #
+# ENet - Real Time Semantic Segmentation            #
+# Paper: https://arxiv.org/pdf/1606.02147.pdf       #
+#                                                   #
+# Anyone is allowed to modify this code with proper #
+# credits directing back to this repository.        #
+#####################################################
+
+class ENet(nn.Module):
+
+    def __init__(self, C):
+        """
+        This is the constructor class of ENet
+
+        Arguments:
+
+        C - int - The number of total number of classes 
+                  that are present in the dataset.
+        """
+
+        super().__init__()
+        
+        # Define class variables
+        self.C = C
+        
+        self.init = InitialBlock()
+        
+        self.b10 = RDDNeck(256, 256, 1, 16, 64, True, 0.01)
+        self.b11 = RDDNeck(128, 128, 1, 64, 64, False, 0.01)
+        self.b12 = RDDNeck(128, 128, 1, 64, 64, False, 0.01)
+        self.b13 = RDDNeck(128, 128, 1, 64, 64, False, 0.01)
+        self.b14 = RDDNeck(128, 128, 1, 64, 64, False, 0.01)
+        
+        self.b20 = RDDNeck(128, 128, 1, 64, 128, True)
+        self.b21 = RDDNeck(64, 64, 1, 128, 128, False)
+        self.b22 = RDDNeck(64, 64, 2, 128, 128, False)
+        self.b23 = ASNeck(64, 64, 128, 128)
+        self.b24 = RDDNeck(64, 64, 4, 128, 128, False)
+        self.b25 = RDDNeck(64, 64, 1, 128, 128, False)
+        self.b26 = RDDNeck(64, 64, 8, 128, 128, False)
+        self.b27 = ASNeck(64, 64, 128, 128)
+        self.b28 = RDDNeck(64, 64, 16, 128, 128, False)
+        
+        self.b31 = RDDNeck(64, 64, 1, 128, 128, False)
+        self.b32 = RDDNeck(64, 64, 2, 128, 128, False)
+        self.b33 = ASNeck(64, 64, 128, 128)
+        self.b34 = RDDNeck(64, 64, 4, 128, 128, False)
+        self.b35 = RDDNeck(64, 64, 1, 128, 128, False)
+        self.b36 = RDDNeck(64, 64, 8, 128, 128, False)
+        self.b37 = ASNeck(64, 64, 128, 128)
+        self.b38 = RDDNeck(64, 64, 16, 128, 128, False)
+        
+        self.b40 = UBNeck(64, 64, 128, 64)
+        self.b41 = RDDNeck(128, 128, 1, 64, 64, False)
+        self.b42 = RDDNeck(128, 128, 1, 64, 64, False)
+        
+        self.b51 = UBNeck(128, 128, 64, 16)
+        self.b52 = RDDNeck(256, 256, 1, 16, 16, False)
+        
+        self.fullconv = nn.Conv2d(16, self.C, 1)
+        
+        
+    def forward(self, x):
+        """
+        The function that defines the forward pass through the ENet
+
+        Arguments:
+
+        x - torch.tensor - The input to the network
+                           The input is assumed to be in shape [N, C, H, W]
+                           where,
+                           N - The Batch size
+                           C - The number of channels (ususally 3)
+                           H - The height dimension of the tensor
+                           W - The width dimension of the tensor
+        """
+        
+        x = self.init(x)
+        
+        x, i1 = self.b10(x)
+        x = self.b11(x)
+        x = self.b12(x)
+        x = self.b13(x)
+        x = self.b14(x)
+        
+        x, i2 = self.b20(x)
+        x = self.b21(x)
+        x = self.b22(x)
+        x = self.b23(x)
+        x = self.b24(x)
+        x = self.b25(x)
+        x = self.b26(x)
+        x = self.b27(x)
+        x = self.b28(x)
+        
+        x = self.b31(x)
+        x = self.b32(x)
+        x = self.b33(x)
+        x = self.b34(x)
+        x = self.b35(x)
+        x = self.b36(x)
+        x = self.b37(x)
+        x = self.b38(x)
+        
+        x = self.b40(x, i2)
+        x = self.b41(x)
+        x = self.b42(x)
+        
+        x = self.b50(x, i1)
+        x = self.b51(x)
+        
+        x = self.fullconv(x)
+        
+        return x
---
+++ models/rddneck.py
@@ -0,0 +1,99 @@
+class RDDNeck(nn.Module):
+    def __init__(self, h, w, dilate, in_channels, out_channels, down_flag, p=0.1):
+        
+        super().__init__()
+        
+        # Define class variables
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.h = h
+        self.w = w
+        self.dilate = dilate
+        self.down_flag = down_flag
+
+        if down_flag:
+            self.stride = 2
+        else:
+            self.stride = 1
+          
+        
+        self.maxpool = nn.MaxPool2d(kernel_size = 2,
+                                      stride = 2,
+                                      padding = 0, return_indices=True)
+        
+
+        
+        self.dropout = nn.Dropout2d(p=p)
+        
+        self.conv1 = nn.Conv2d(in_channels = self.in_channels,
+                               out_channels = self.out_channels,
+                               kernel_size = 1,
+                               stride = 1,
+                               padding = 0,
+                               bias = False,
+                               dilation = 1)
+        
+        self.prelu1 = nn.PReLU()
+        
+        self.conv2 = nn.Conv2d(in_channels = self.out_channels,
+                                  out_channels = self.out_channels,
+                                  kernel_size = 3,
+                                  stride = self.stride,
+                                  padding = self.dilate,
+                                  bias = True,
+                                  dilation = self.dilate)
+        
+        self.prelu2 = nn.PReLU()
+        
+        self.conv3 = nn.Conv2d(in_channels = self.out_channels,
+                                  out_channels = self.out_channels,
+                                  kernel_size = 1,
+                                  stride = 1,
+                                  padding = 0,
+                                  bias = False,
+                                  dilation = 1)
+        
+        self.prelu3 = nn.PReLU()
+        
+        self.batchnorm = nn.BatchNorm2d(self.out_channels)
+        
+    def forward(self, x):
+        
+        bs = x.size()[0]
+        x_copy = x.clone()
+        
+        # Side Branch
+        x = self.conv1(x)
+        x = self.batchnorm(x)
+        x = self.prelu1(x)
+        
+        x = self.conv2(x)
+        x = self.batchnorm(x)
+        x = self.prelu2(x)
+        
+        x = self.conv3(x)
+        x = self.batchnorm(x)
+                
+        x = self.dropout(x)
+        
+        # Main Branch
+        
+        if self.down_flag:
+            x_copy, indices = self.maxpool(x_copy)
+          
+        if self.in_channels != self.out_channels:
+            out_shape = self.out_channels - self.in_channels
+            extras = torch.zeros((bs, out_shape, x.shape[2], x.shape[3]))
+            print (x_copy.shape, extras.shape)
+            x_copy = torch.cat((x_copy, extras), dim = 1)
+            
+        print (x_copy.shape)
+        
+        # Sum of main and side branches
+        x = x + x_copy
+        x = self.prelu3(x)
+        
+        if self.down_flag:
+            return x, indices
+        else:
+            return x
