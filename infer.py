ca55391761d21ee2c54db95bdd2f0ff2d20799a0
Arunava <learningdroidarunava@gmail.com>
2019-01-24 17:02:37 +0530

    Addded arguments to the init file;'

---
+++ infer.py
@@ -0,0 +1,124 @@
+from utils import *
+
+def train(FLAGS):
+
+    # Defining the hyperparameters
+	cuda =  FLAGS.cuda
+    batch_size = FLAGS.batch_size
+    epochs = FLAGS.epochs
+    lr = FLAGS.learning_rate
+    print_every = FLAGS.print_every
+    eval_every = FLAGS.eval_every
+    save_every = FLAGS.save_every
+    nc = FLAGS.num_classes
+    wd = FLAGS.weight_decay
+    ip = FLAGS.input_path
+    lp = FLAGS.label_path
+    ipv = FLAGS.input_path_val
+    lpv = FLAGS.label_path_val
+    print ('[INFO]Defined all the hyperparameters successfully!')
+    
+    # Get the class weights
+    print ('[INFO]Starting to define the class weights...')
+	pipe = loader(ip, lp, batch_size='all')
+    class_weights = get_class_weights(pipe, nc)
+    print ('[INFO]Fetched all class weights successfully!')
+
+    # Get an instance of the model
+    enet = ENet(nc)
+    print ('[INFO]Model Instantiated!')
+
+    # Define the criterion and the optimizer
+    criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor(class_weights).to(device))
+    optimizer = torch.optim.Adam(enet.parameters(),
+                                 lr=lr,
+                                 weight_decay=wd)
+    print ('[INFO]Defined the loss function and the optimizer')
+
+    # Training Loop starts
+    print ('[INFO]Staring Training...')
+    print ()
+
+	train_losses = []
+	eval_losses = []
+	
+	# Assuming we are using the CamVid Dataset
+	bc_train = 367 // batch_size
+	bc_eval = 101 // batch_size
+
+	pipe = loader(ip, lp, batch_size)
+	eval_pipe = loader(ipv, lpv, batch_size)
+
+	epochs = epochs
+		
+	for e in range(1, epochs+1):
+		
+		
+		train_loss = 0
+		print ('-'*15,'Epoch %d' % e, '-'*15)
+		
+		enet.train()
+		
+		for _ in tqdm(range(bc_train)):
+		    X_batch, mask_batch = next(pipe)
+		    
+		    #assert (X_batch >= 0. and X_batch <= 1.0).all()
+		    
+		    if cuda:
+		        X_batch, mask_batch = X_batch.cuda(), mask_batch.cuda()
+
+		    optimizer.zero_grad()
+
+		    out = enet(X_batch.float())
+
+		    loss = criterion(out, mask_batch.long())
+		    loss.backward()
+		    optimizer.step()
+
+		    train_loss += loss.item()
+
+		    
+		print ()
+		train_losses.append(train_loss)
+		
+		if (e+1) % print_every == 0:
+		    print ('Epoch {}/{}...'.format(e, epochs),
+		            'Loss {:6f}'.format(train_loss))
+		
+		if e % eval_every == 0:
+		    with torch.no_grad():
+		        enet.eval()
+		        
+		        eval_loss = 0
+		        
+		        for _ in tqdm(range(bc_eval)):
+		            inputs, labels = next(eval_pipe)
+
+		            
+		            inputs, labels = inputs.to(device), labels.to(device)
+		                
+		            
+		            out = enet(inputs)
+		            
+		            out = out.data.max(1)[1]
+		            
+		            eval_loss += (labels.long() - out.long()).sum()
+		            
+		        
+		        print ()
+		        print ('Loss {:6f}'.format(eval_loss))
+		        
+		        eval_losses.append(eval_loss)
+		    
+		if e % save_every == 0:
+		    checkpoint = {
+		        'epochs' : e,
+		        'state_dict' : enet.state_dict()
+		    }
+		    torch.save(checkpoint, '/content/ckpt-enet-{}-{}.pth'.format(e, train_loss))
+		    print ('Model saved!')
+
+	print ('Epoch {}/{}...'.format(e+1, epochs),
+		   'Total Mean Loss: {:6f}'.format(sum(train_losses) / epochs))
+
+    print ('[INFO]Training Process complete!')
--- init.py
+++ init.py
@@ -38,12 +38,92 @@ if __name__ == '__main__':
 
     parser.add_argument('-m', '--model-path',
                         type=str,
-                        default='./datasets/CamVid/ckpt-camvid-enet.py',
+                        default='./datasets/CamVid/ckpt-camvid-enet.pth',
                         help='The path to the pretrained enet model')
 
     parser.add_argument('-i', '--image-path',
                         type=str,
                         help='The path to the image to perform semantic segmentation')
+
+    parser.add_argument('-lr', '--learning-rate',
+                        type=float,
+                        default=5e-4,
+                        help='The learning rate')
+
+    parser.add_argument('-bs', '--batch-size',
+                        type=int,
+                        default=10,
+                        help='The batch size')
+
+    parser.add_argument('-wd', '--weight-decay',
+                        type=float,
+                        default=2e-4,
+                        help='The weight decay')
+
+    parser.add_argument('-c', '--constant',
+                        type=float,
+                        default=1.02,
+                        help='The constant used for calculating the class weights')
+
+    parser.add_argument('-e', '--epochs',
+                        type=int,
+                        default=102,
+                        help='The number of epochs')
+
+    parser.add_argument('-nc', '--num-classes',
+                        type=int,
+                        default=102,
+                        help='The number of epochs')
+
+    parser.add_argument('-se', '--save-every',
+                        type=int,
+                        default=10,
+                        help='The number of epochs after which to save a model')
+
+    parser.add_argument('-iptr', '--input-path-train',
+                        type=str,
+                        default='./datasets/CamVid/train/',
+                        help='The path to the input dataset')
+
+    parser.add_argument('-lptr', '--label-path-train',
+                        type=str,
+                        default='./datasets/CamVid/trainanot/',
+                        help='The path to the label dataset')
+
+    parser.add_argument('-ipv', '--input-path-val',
+                        type=str,
+                        default='./datasets/CamVid/val/',
+                        help='The path to the input dataset')
+
+    parser.add_argument('-lpv', '--label-path-val',
+                        type=str,
+                        default='./datasets/CamVid/valanot/',
+                        help='The path to the label dataset')
+
+    parser.add_argument('-iptt', '--input-path-test',
+                        type=str,
+                        default='./datasets/CamVid/test/',
+                        help='The path to the input dataset')
+
+    parser.add_argument('-lptt', '--label-path-test',
+                        type=str,
+                        default='./datasets/CamVid/testanot/',
+                        help='The path to the label dataset')
+
+    parser.add_argument('-pe', '--print-every',
+                        type=int,
+                        default=1,
+                        help='The number of epochs after which to print the training loss')
+
+    parser.add_argument('-ee', '--eval-every',
+                        type=int,
+                        default=1,
+                        help='The number of epochs after which to print the validation loss')
+
+    parser.add_argument('--cuda',
+                        type=bool,
+                        default=False,
+                        help='Whether to use cuda or not')
 
     parser.add_argument('--mode'
                         choices=['train', 'test'],
