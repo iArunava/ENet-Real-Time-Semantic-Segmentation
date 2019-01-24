68712726cd261a5b4899a606537e3fc7064d72d7
Arunava <learningdroidarunava@gmail.com>
2019-01-24 14:47:16 +0530

    Updated the utils file

--- utils.py
+++ utils.py
@@ -64,23 +64,26 @@ def loader(training_path, segmented_path, batch_size):
     
     assert(total_files_t == total_files_s)
     
-    iterations = (total_files_t // batch_size) + 1 if total_files_t % batch_size != 0 else (total_files_t // batch_size)
+    if str(batch_size).lower() == 'all':
+        batch_size = total_files_s
+    
     idx = 0
-    for ii in range(iterations):
-        new_idx = idx + batch_size if idx+batch_size < total_files_t else total_files_t
+    while(1):
+        batch_idxs = np.random.randint(0, total_files_s, batch_size)
+            
+        
         inputs = []
         labels = []
         
-        for jj in range(idx, new_idx):
+        for jj in batch_idxs:
             img = plt.imread(training_path + filenames_t[jj])
-            img = cv2.resize(img, (512, 512), cv2.INTER_CUBIC)
+            img = cv2.resize(img, (h, w), cv2.INTER_NEAREST)
             inputs.append(img)
             
-            img = cv2.imread(segmented_path + filenames_s[jj])
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img, (512, 512), cv2.INTER_NEAREST)
-            mask = create_mask(img, color_map)
-            labels.append(mask)
+            img = Image.open(segmented_path + filenames_s[jj])
+            img = np.array(img)
+            img = cv2.resize(img, (h, w), cv2.INTER_NEAREST)
+            labels.append(img)
          
         inputs = np.stack(inputs, axis=2)
         inputs = torch.tensor(inputs).transpose(0, 2).transpose(1, 3)
@@ -88,5 +91,60 @@ def loader(training_path, segmented_path, batch_size):
         labels = torch.tensor(labels)
         
         yield inputs, labels
-        
-        idx = new_idx
+
+
+def decode_segmap(image):
+	Sky = [128, 128, 128]
+    Building = [128, 0, 0]
+    Pole = [192, 192, 128]
+    Road_marking = [255, 69, 0]
+    Road = [128, 64, 128]
+    Pavement = [60, 40, 222]
+    Tree = [128, 128, 0]
+    SignSymbol = [192, 128, 128]
+    Fence = [64, 64, 128]
+    Car = [64, 0, 128]
+    Pedestrian = [64, 64, 0]
+    Bicyclist = [0, 128, 192]
+
+    label_colors = np.array([Sky, Building, Pole, Road_marking, Road, 
+                              Pavement, Tree, SignSymbol, Fence, Car, 
+                              Pedestrian, Bicyclist]).astype(np.uint8)
+
+	r = np.zeros_like(image).astype(np.uint8)
+	g = np.zeros_like(image).astype(np.uint8)
+	b = np.zeros_like(image).astype(np.uint8)
+
+	for label in range(len(label_colors)):
+		r[image == label] = label_colors[l, 0]
+		g[image == label] = label_colors[l, 1]
+		b[image == label] = label_colors[l, 2]
+
+	rgb = np.zeros((image.shape[0], image.shape[1], 3)).astype(np.uint8)
+	rgb[:, :, 0] = b
+	rgb[:, :, 1] = g
+	rgb[:, :, 2] = r
+
+	return rgb
+
+def show3(img1, img2, img3, in_row=True):
+	'''
+	Helper function to show 3 images
+	'''
+	if not in_row:
+		rc_tuple = (3, 1)
+
+	figure = plt.figure(figsize=(20, 10))
+	plt.subplot(*rc_tuple, 1)
+	plt.title('Input Image')
+	plt.axis('off')
+	plt.imshow(img1)
+	plt.subplot(*rc_tuple, 2)
+	plt.title('Predicted Segmentation')
+	plt.axis('off')
+	plt.imshow(img2)
+	plt.subplot(*rc_tuple, 3)
+	plt.title('Ground Truth')
+	plt.axis('off')
+	plt.imshow(img3)
+	plt.show()
